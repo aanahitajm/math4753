552322569e377ac3093f16277d0d19bafb4e6feb
aanahitajm <aanahitajm@gmail.com>
2022-03-07 12:23:42 -0600

    Added FIREDAM data set and documented it

---
+++ R/FIREDAM.R
@@ -0,0 +1,12 @@
+#' FIREDAM data sets for fires that occurred
+#'
+#' A dataset containing the data for 15 fires which document the damages sustained by a fire at a certain distance.
+#'
+#' @format A data frame with 15 rows and 2 variables:
+#' \describe{
+#'   \item{DISTANCE}{distance, in units of miles}
+#'   \item{DAMAGE}{damage, in dollars}
+#'   ...
+#' }
+#' @source text Book
+"fire"
